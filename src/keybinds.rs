d2deb0332bc97ccf1e9db1f9dfc5b56205667b5c
wilovy <emiliano@adquiere.co>
2026-04-14 00:28:52 -0600

    feat(keybinds): expand and centralize keymap for all UI actions

--- src/keybinds.rs
+++ src/keybinds.rs
@@ -22,19 +22,83 @@ impl KeyBinding {
 pub struct KeyMap {
     pub quit: KeyBinding,
     pub help: KeyBinding,
+    pub send: KeyBinding,
+    pub save: KeyBinding,
+    pub tab_next: KeyBinding,
+    pub tab_prev: KeyBinding,
+    pub up: KeyBinding,
+    pub down: KeyBinding,
+    pub new_item: KeyBinding,
+    pub delete_item: KeyBinding,
+    pub toggle_section: KeyBinding,
+    pub confirm: KeyBinding,
+    pub cancel: KeyBinding,
+    pub method_next: KeyBinding,
+    pub method_prev: KeyBinding,
+    pub new_folder: KeyBinding,
 }
 
 impl KeyMap {
     pub fn new() -> Self {
         Self {
             quit: KeyBinding::new(
-                "Exit program",
+                "Quit",
+                vec![(KeyCode::Char('q'), KeyModifiers::CONTROL)],
+            ),
+            help: KeyBinding::new("Toggle help", vec![(KeyCode::Char('?'), KeyModifiers::NONE)]),
+            send: KeyBinding::new(
+                "Send request",
                 vec![
-                    (KeyCode::Char('q'), KeyModifiers::NONE),
-                    (KeyCode::Esc, KeyModifiers::NONE),
+                    (KeyCode::F(5), KeyModifiers::NONE),
+                    (KeyCode::Enter, KeyModifiers::CONTROL),
                 ],
             ),
-            help: KeyBinding::new("Show help", vec![(KeyCode::Char('?'), KeyModifiers::NONE)]),
+            save: KeyBinding::new(
+                "Save request",
+                vec![(KeyCode::Char('s'), KeyModifiers::CONTROL)],
+            ),
+            tab_next: KeyBinding::new("Focus next panel", vec![(KeyCode::Tab, KeyModifiers::NONE)]),
+            tab_prev: KeyBinding::new(
+                "Focus previous panel",
+                vec![(KeyCode::BackTab, KeyModifiers::SHIFT)],
+            ),
+            up: KeyBinding::new(
+                "Move up",
+                vec![
+                    (KeyCode::Char('k'), KeyModifiers::NONE),
+                    (KeyCode::Up, KeyModifiers::NONE),
+                ],
+            ),
+            down: KeyBinding::new(
+                "Move down",
+                vec![
+                    (KeyCode::Char('j'), KeyModifiers::NONE),
+                    (KeyCode::Down, KeyModifiers::NONE),
+                ],
+            ),
+            new_item: KeyBinding::new("New item", vec![(KeyCode::Char('n'), KeyModifiers::NONE)]),
+            delete_item: KeyBinding::new(
+                "Delete item",
+                vec![(KeyCode::Char('d'), KeyModifiers::NONE)],
+            ),
+            toggle_section: KeyBinding::new(
+                "Toggle sidebar section",
+                vec![(KeyCode::Char('s'), KeyModifiers::NONE)],
+            ),
+            confirm: KeyBinding::new("Confirm", vec![(KeyCode::Enter, KeyModifiers::NONE)]),
+            cancel: KeyBinding::new("Cancel / close", vec![(KeyCode::Esc, KeyModifiers::NONE)]),
+            method_next: KeyBinding::new(
+                "Next HTTP method",
+                vec![(KeyCode::Right, KeyModifiers::NONE), (KeyCode::Char('l'), KeyModifiers::NONE)],
+            ),
+            method_prev: KeyBinding::new(
+                "Prev HTTP method",
+                vec![(KeyCode::Left, KeyModifiers::NONE), (KeyCode::Char('h'), KeyModifiers::NONE)],
+            ),
+            new_folder: KeyBinding::new(
+                "New folder",
+                vec![(KeyCode::Char('f'), KeyModifiers::NONE)],
+            ),
         }
     }
 
@@ -42,6 +106,18 @@ impl KeyMap {
         vec![
             (self.quit.description, self.quit.keys.clone()),
             (self.help.description, self.help.keys.clone()),
+            (self.send.description, self.send.keys.clone()),
+            (self.save.description, self.save.keys.clone()),
+            (self.tab_next.description, self.tab_next.keys.clone()),
+            (self.tab_prev.description, self.tab_prev.keys.clone()),
+            (self.up.description, self.up.keys.clone()),
+            (self.down.description, self.down.keys.clone()),
+            (self.new_item.description, self.new_item.keys.clone()),
+            (self.delete_item.description, self.delete_item.keys.clone()),
+            (self.toggle_section.description, self.toggle_section.keys.clone()),
+            (self.method_next.description, self.method_next.keys.clone()),
+            (self.method_prev.description, self.method_prev.keys.clone()),
+            (self.new_folder.description, self.new_folder.keys.clone()),
         ]
     }
 }
