0d5bc45762e70dea4fc7e5f2f82011e0f6dd4ccc
wilovy <emiliano@adquiere.co>
2026-04-14 00:28:52 -0600

    refactor(widgets): modularize and export all core widgets

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,5 +1,20 @@
-pub mod sidebar;
-pub use sidebar::*;
-
 pub mod help_modal;
 pub use help_modal::*;
+
+pub mod input;
+pub use input::TextInput;
+
+pub mod modal;
+pub use modal::Modal;
+
+pub mod request_panel;
+pub use request_panel::{RequestPanel, RequestPanelState, RequestTab};
+
+pub mod response_panel;
+pub use response_panel::ResponsePanel;
+
+pub mod sidebar;
+pub use sidebar::{FlatNodeKind, Sidebar, SidebarSection, SidebarState};
+
+pub mod toolbar;
+pub use toolbar::{Toolbar, ToolbarFocus};
